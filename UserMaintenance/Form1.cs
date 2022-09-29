a949009f375f1cad95a9233f264287dea0a8b5b8
bce-benedek-kalmar <benedek.kalmar@stud.uni-corvinus.hu>
2022-09-29 19:22:01 +0200

    GUI and base objects created

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            labelLastName.Text = Resource1.LastName;
+            labelFirstName.Text = Resource1.FirstName;
+            btnAdd.Text = Resource1.Add;
         }
     }
 }
