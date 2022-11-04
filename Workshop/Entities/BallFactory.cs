00be7c31248dfc145c5cfe5c3ac2ef381f6ea447
Benedek Kalmár <benedek.kalmar@stud.uni-corvinus.hu>
2022-11-04 12:38:00 +0100

    Added conveyor belt for Balls

--- Workshop/Entities/BallFactory.cs
+++ Workshop/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Workshop.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
--- Workshop/Form1.cs
+++ Workshop/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Workshop.Entities;
 
 namespace Workshop
 {
@@ -15,6 +16,48 @@ namespace Workshop
         public Form1()
         {
             InitializeComponent();
+
+            Factory = new BallFactory();
+        }
+
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Ball b = Factory.CreateNew();
+            _balls.Add(b);
+            mainPanel.Controls.Add(b);
+            b.Left = -b.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var rightestBallPosition = 0;
+
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+
+                if (ball.Left>rightestBallPosition)
+                {
+                    rightestBallPosition = ball.Left;
+                }
+            }
+
+            if (rightestBallPosition>=1000)
+            {
+                var outBall = _balls[0];
+                _balls.Remove(outBall);
+                mainPanel.Controls.Remove(outBall);
+            }
         }
     }
 }
