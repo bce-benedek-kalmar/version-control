3a1bad7d79a194c179ccc8a88299b11e56fbb9af
Benedek Kalmár <benedek.kalmar@stud.uni-corvinus.hu>
2022-11-04 12:57:32 +0100

    Added Car toy

---
+++ Workshop/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workshop.Abstractions;
+
+namespace Workshop.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imagefile = Image.FromFile("Images/car.png");
+            graphics.DrawImage(imagefile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
