e035ffc06d373c59c172e725ccf9d2e89562c8f0
Benedek Kalmár <benedek.kalmar@stud.uni-corvinus.hu>
2022-11-25 13:12:34 +0100

    Added testing for registration

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -53,6 +54,57 @@ namespace UnitTestExample.Test
             //Assert
 
             Assert.AreEqual(expectedResult, result);
+        }
+
+        [
+            Test,
+            TestCase("info@tejivo.hu", "Abcd12345"),
+            TestCase("boci@tejivo.hu", "Password12345")
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            //Arrange
+
+            var accountController = new AccountController();
+
+            //Act
+
+            var result = accountController.Register(email, password);
+
+            //Assert
+
+            Assert.AreEqual(email, result.Email);
+            Assert.AreEqual(password, result.Password);
+            Assert.AreNotEqual(Guid.Empty, result.ID);
+        }
+
+
+        [
+            Test,
+            TestCase("info@tejivo", "Abcd1234"),
+            TestCase("info.tejivo.hu", "Abcd1234"),
+            TestCase("info@tejivo.hu", "abcd1234"),
+            TestCase("info@tejivo.hu", "ABCD1234"),
+            TestCase("info@tejivo.hu", "abcdABCD"),
+            TestCase("info@tejivo.hu", "Ab1234"),
+        ]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            //Arrange
+
+            var accountController = new AccountController();
+
+            //Act
+
+            try
+            {
+            var result = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
         }
     }
 }
