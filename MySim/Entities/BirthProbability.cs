afb0e19e7d7066a11b12f1dff32781f4025cea7f
Benedek Kalmár <benedek.kalmar@stud.uni-corvinus.hu>
2022-12-02 12:15:45 +0100

    Created base entities

---
+++ MySim/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySim.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public byte NumChildren { get; set; }
+        public double Probability { get; set; }
+        
+    }
+}
---
+++ MySim/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySim.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NumChildren { get; set; }
+        public bool Living { get; set; }
+
+        public Person()
+        {
+            Living = true;
+        }
+    }
+}
